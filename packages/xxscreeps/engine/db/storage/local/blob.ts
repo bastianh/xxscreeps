8c09c6e7e88ea480189fd7c04946b61dd83e79f0
Bastian Hoyer <dafire@gmail.com>
2026-07-12 09:14:55 +0200

    storage: tolerate unlink of never-flushed blobs on save
    A blob created and deleted between two saves never made it to disk;
    save() then failed the whole flush with ENOENT trying to unlink it.

--- packages/xxscreeps/engine/db/storage/local/blob.ts
+++ packages/xxscreeps/engine/db/storage/local/blob.ts
@@ -288,7 +288,13 @@ export class BlobStorage extends AsyncDisposableResource {
 				await fs.writeFile(tmp, value);
 				await fs.rename(tmp, path);
 			} else {
-				await fs.unlink(path);
+				// The blob may have been created and deleted between two saves, in
+				// which case it never made it to disk
+				await fs.unlink(path).catch((err: { code?: string }) => {
+					if (err.code !== 'ENOENT') {
+						throw err;
+					}
+				});
 			}
 		});
 
